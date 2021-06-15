f3e57e2011264ba57a72793892ca86742d5b893e
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-15 18:22:12 +0700

    Add RandomNumber event

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import HobbyList from '../../components/Home/HobbyList';
+import { addNewHobby } from '../../actions/hobby';
 
 Homepage.propTypes = {
     
 };
 
+const RandomNumber = () => {
+    return 1000 + Math.trunc(Math.random() * 9000);
+}
+
 function Homepage(props) {
-    const hobbyList = useSelector(state => state.list);
+    const hobbyList = useSelector(state => state.hobby.list);
+    const activeId = useSelector(state => state.hobby.activeId);
+    const dispatch = useDispatch();
+
     console.log('Hobby list:', hobbyList);
+
+    const newId = RandomNumber();
+
+    const handleAddHobbyClick = () => {
+        // Random a hobby object: id + title
+        const newHobby = {
+            id: newId,
+            title: `Hobby ${newId}`
+        };
+        // Dispatch action to add a new hobby to redux store
+        const action = addNewHobby(newHobby);
+        dispatch(action);
+    }
 
     return (
         <div className="home-page">
             <h1>REDUX-HOOKS Home-page!</h1>
+            <button onClick={handleAddHobbyClick}>Random</button>
+            <HobbyList hobbylist={hobbyList}/>
         </div>
     );
 }
