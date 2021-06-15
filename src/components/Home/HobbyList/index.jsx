e7a7480c3e9a90562fe959f645c6f6fd4c1b469f
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-15 21:59:20 +0700

    Add Css for Hobbylist

--- src/components/Home/HobbyList/index.jsx
+++ src/components/Home/HobbyList/index.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './HobbyList.css';
 
 HobbyList.propTypes = {
     hobbylist: PropTypes.array,
     activeid: PropTypes.number,
+    onHobbyClick: PropTypes.func,
 };
 
 HobbyList.defaultProps = {
     hobbylist: [],
-    activeid: 0
+    activeid: 0,
+    onHobbyClick: null
 };
 
 function HobbyList(props) {
-    const { hobbylist, activeid } = props;
+    const { hobbylist, activeid, onHobbyClick } = props;
+
+    const handleClick = (hobby) => {
+        if(onHobbyClick) {
+            onHobbyClick(hobby);
+        }
+    }
+
     return (
         <ul className="hobby-list">
             {hobbylist.map(hobby => (
-                <li key={hobby.id}>{hobby.title}</li>
+                <li 
+                key={hobby.id}
+                className={hobby.id === activeid ? 'active' : ''}
+                onClick={() => handleClick(hobby)}
+                >
+                    {hobby.title}
+                </li>
             ))}
         </ul>
     );
